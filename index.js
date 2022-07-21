bdd443c89112139876a218cdddd09b9a7532a491
Cadence Chiappe <cadence@chiappe.com>
2022-07-21 15:47:47 -0500

    fixed dependencies, iquirer successfully prompts, generates html when run

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require ('./utils/generateMarkdown');
+// const generateMarkdown = require ('./utils/generateMarkdown');
 
 // inquirer prompt and run code
-const promptUser = () => {
-return inquirer.prompt([
-    {
-        type: 'input',
-        name: 'employeeName',
-        message: 'What is your name?',
-    },
-    {
-        type: 'input',
-        name: 'employeeID',
-        message: 'What is your Employee ID?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address?',
-    },
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'What is your office number?',
-    },
-    {
-        type: 'list',
-        name: 'title',
-        message: 'What is your job title?',
-        choices: ["Manager", "Engineer", "Intern"]
-    },
-    ]);
-};
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'employeeName',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input',
+            name: 'employeeID',
+            message: 'What is your Employee ID?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is your office number?',
+        },
+        {
+            type: 'list',
+            name: 'title',
+            message: 'What is your job title?',
+            choices: ["Manager", "Engineer", "Intern"]
+        },
+    ])
+    .then((answers) => {
+        const htmlPageContent = generateHTML(answers);
+    
+        fs.writeFile('index.html', htmlPageContent, (err) =>
+          err ? console.log(err) : console.log('Successfully created index.html!')
+        );
+      });
   
 const generateHTML = ({ employeeName, employeeID, email, officeNumber, title }) =>
     `<!DOCTYPE html>
@@ -65,14 +71,14 @@ const generateHTML = ({ employeeName, employeeID, email, officeNumber, title }) 
     </body>
   </html>`;
   
-  // Bonus using writeFileSync as a promise
-  const init = () => {
-    promptUser()
-      // Use writeFileSync method to use promises instead of a callback function
-      .then((answers) => fs.writeFileSync('index.html', generateHTML(answers)))
-      .then(() => console.log('Successfully wrote to index.html'))
-      .catch((err) => console.error(err));
-};
+//   // Bonus using writeFileSync as a promise
+//   const init = () => {
+//     promptUser()
+//       // Use writeFileSync method to use promises instead of a callback function
+//       .then((answers) => fs.writeFileSync('index.html', generateHTML(answers)))
+//       .then(() => console.log('Successfully wrote to index.html'))
+//       .catch((err) => console.error(err));
+// };
 
-init();
+// init();
   
